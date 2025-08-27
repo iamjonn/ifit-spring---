4afa9dd8b4f14c4878d08aa3a08b132c752e6441
Jonata Nascimento Barbosa <110827553+iamjonn@users.noreply.github.com>
2025-08-26 21:38:54 -0300

    Delete ifit-angular-main directory

--- ifit-angular-main/ifit-app/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config'; // <-- Importe o appConfig
-
-bootstrapApplication(AppComponent, appConfig) // <-- Use o appConfig completo aqui
-  .catch((err) => console.error(err));
